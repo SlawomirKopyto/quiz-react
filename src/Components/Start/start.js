4005ceb3b04322dd7bea5bba4bdd93d5e4e584f4
Slawomir Kopyto <slawomirkopyto@gmail.com>
2022-02-23 20:45:06 +0100

    initial commit

---
+++ src/Components/Start/start.js
@@ -0,0 +1,21 @@
+import './start.css'
+const Start = ({ onQuizStart }) => {
+    return (
+      <div className="card">
+        <div className="card-content">
+          <div className="content">
+            <h1>Witamy w Quizie</h1>
+            <h4>Zasady</h4>
+            <ol className='card-description'>
+              <li>Każde pytanie ma cztery odpowiedzi z jedną poprawną</li>
+              <li>Po wybraniu odpowiedzi nie można jej cofnąć</li>
+              <li>Po 5 minutach nieaktywności powrót do ekranu głównego</li>
+            </ol>
+            <p>Powodzenia</p>
+            <button className="button" onClick={onQuizStart}>Start</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  export default Start;
