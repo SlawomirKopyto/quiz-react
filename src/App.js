b5c151b3b554c47ce6982c634025bb13760441bb
Slawomir Kopyto <slawomirkopyto@gmail.com>
2022-02-24 22:04:41 +0100

    added question card and json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import  {useState } from 'react';
 import './App.css';
 import Start from './Components/Start/start';
+import Question from './Components/Question/question';
+import PLNQuestion from './Data/pln.json';
 
 const App = () => {
+  const [step, setStep] = useState(1);
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
+
+  const quizStartHandler = () => {
+    setStep(2);
+  }
+
   return (
     <div className="App">
-      <Start />
+      {step === 1 && <Start onQuizStart={quizStartHandler} />}
+      {step === 2 && <Question 
+      data={PLNQuestion.data[activeQuestion]}
+      onAnswerUpdate={setAnswers}
+      numberOffQuestion={PLNQuestion.data.length}
+      activeQuestion={activeQuestion}
+      onSetActiveQuestion={setActiveQuestion}
+      onSetStep={setStep}
+      />}
     </div>
   );
 }
---
+++ src/Components/Question/question.js
@@ -0,0 +1,58 @@
+import { useState, useRef } from "react";
+import './question.css'
+
+const Question = ({ data, onAnswerUpdate, numberOffQuestion, activeQuestion, onSetActiveQuestion, onSetStep, onReset}) => {
+    const [selected, setSelected] = useState();
+  const [error, setError] = useState('');
+  const radioWrapper = useRef();
+
+  const handleSelect = (option) => {
+    if (selected === option && selected === data.answer) return "select";
+    else if (selected === option && selected !== data.answer) return "wrong";
+    else if (option === data.answer) return "select";
+  };
+
+  const changeHandler = (e) => {
+    setSelected(e.target.value);
+    if (error) {
+      setError('');
+    }
+  }
+
+  const nextClickHandler = () => {
+    if (selected === '') {
+      return setError('Zaznacz jakąś odpowiedź!')
+    }
+    onAnswerUpdate(prevState => [...prevState, { q: data.question, a: selected }]);
+    setSelected('');
+    if (activeQuestion < numberOffQuestion - 1) {
+      onSetActiveQuestion(activeQuestion + 1);
+    } else {
+      onSetStep(3);
+    }
+  }
+    return (
+        <div className="card">
+          <div className="image"></div>
+          <div className="card-content">
+            <div className="content">
+              <h2 className="">{data.question}</h2>
+              <div className="control" ref={radioWrapper}>
+                {data.options.map((option, i) => (
+                  <label  className={`singleOption  ${selected && handleSelect(option)}`} key={i}>
+                    <input type="radio" name="answer" value={option} onChange={changeHandler} disabled={selected}  />
+                    {option}
+                  </label>
+                ))}
+              </div>
+              {error && <div className="text-danger">{error}</div>}
+              <div className="button-navigation">
+                <button className="button" onClick={nextClickHandler}>Dalej</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+}
+
+export default Question;
