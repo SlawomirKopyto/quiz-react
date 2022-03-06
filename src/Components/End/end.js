c5c0a6df91bc0713736fc8b1960e7c678f017d10
Slawomir Kopyto <slawomirkopyto@gmail.com>
2022-03-06 11:31:10 +0100

    added display of text related to knowledge

--- src/Components/End/end.js
+++ src/Components/End/end.js
@@ -2,28 +2,46 @@ import { useState, useEffect } from 'react';
 import './end.css'
 
 const End = ({ results, data, onReset, onAnswersCheck }) => {
-    const [correctAnswers, setCorrectAnswers] = useState(0);
-  
-    useEffect(() => {
-      
-      let correct = 0;
-      results.forEach((result, index) => {
-        if (result.a === data[index].answer) {
-          correct++;
-        }
-      });
-      setCorrectAnswers(correct);
-    }, []);
-return (
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+
+  useEffect(() => {
+
+    let correct = 0;
+    results.forEach((result, index) => {
+      if (result.a === data[index].answer) {
+        correct++;
+      }
+    });
+    setCorrectAnswers(correct);
+  }, []);
+
+  let aa = {
+    ddd: ((correctAnswers / data.length) * 100)
+  }
+
+  function xxx(aa) {
+    if (aa.ddd < 55)
+      return 'Niestety poszło Ci bardzo słabo';
+    else
+      if (aa.ddd > 80)
+        return 'Świetnie, jesteś bardzo dobry';
+      else {
+        return 'Nie jest źle ale może być lepiej';
+      }
+  };
+  xxx(aa);
+
+  return (
     <div className="card">
       <div className="card-content card-end">
         <div className="content">
           <h3>Twój wynik</h3>
           <p>{correctAnswers} z {data.length}</p>
           <p><strong>{Math.floor((correctAnswers / data.length) * 100)}%</strong></p>
+          <p><strong>{xxx(aa)}</strong></p>
           <div className="card-button">
-          <button className="button" onClick={onAnswersCheck}>Sprawdź swoje odpowiedzi</button>
-          <button className="button button-repeat" onClick={onReset}>Spróbuj ponownie</button>
+            <button className="button" onClick={onAnswersCheck}>Sprawdź swoje odpowiedzi</button>
+            <button className="button button-repeat" onClick={onReset}>Spróbuj ponownie</button>
           </div>
         </div>
       </div>
